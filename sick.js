06873c760ed02ccf4790ad46a30ed5631db3e609
Bridget <bridget.dou@gmail.com>
2015-03-22 01:11:30 -0400

    Removing instead of hiding; changing widths

--- sick.js
+++ sick.js
@@ -1,25 +1,40 @@
 function short(el) {
   el.style.color="red";
-  el.style.width= "80%";
+  el.style.width= "87.2%";
   
   var myElements = document.querySelectorAll(".tbshort");
   for (var i = 0; i < myElements.length; i++) {
     myElements[i].style.opacity = 1;
   }
   var bye=document.getElementById("long");
-  bye.style.opacity=0;
+  bye.parentNode.removeChild(bye);
+  var bye1=document.getElementById("CM");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("CAPS");
+  bye2.parentNode.removeChild(bye2);
+  var bye3=document.getElementById("DO");
+  bye3.parentNode.removeChild(bye3);
+  var bye4=document.getElementById("SDAO");
+  bye4.parentNode.removeChild(bye4);
+
 }
 
 
 function long(el) {
   el.style.color="red";
-  el.style.width= "80%";
+  el.style.width= "87.2%";
   
   var myElements = document.querySelectorAll(".tblong");
   for (var i = 0; i < myElements.length; i++) {
     myElements[i].style.opacity = 1;
   }
   var bye=document.getElementById("short");
-  bye.style.opacity=0;
+  bye.parentNode.removeChild(bye);
+  var bye1=document.getElementById("STINF");
+  bye1.parentNode.removeChild(bye1);
+  var bye2=document.getElementById("pharm");
+  bye2.parentNode.removeChild(bye2);
+
+  
   
 }
