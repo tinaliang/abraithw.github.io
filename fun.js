4d30e301ba15e3624648b731cf6b18f133ea7502
abraithw <arb79@duke.edu>
2015-03-22 00:42:39 -0400

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,8 +1,8 @@
 var canvas=Raphael(10,10,750,750);
 
-for (var i=0;i<50;i++){
-  for (var j=0; j<50;j++) {
-  var myCircle=canvas.circle(50+i*15,50+j*15,6);
+for (var i=0;i<40;i++){
+  for (var j=0; j<40;j++) {
+  var myCircle=canvas.circle(50+i*15,50+j*15,7);
   myCircle.attr("fill","blue");  
   myCircle.hover(pearl);
   myCircle.click(hi);
@@ -15,8 +15,8 @@ function hi() {
 
 
 function pearl() {
-  var x = Math.floor(Math.random()*300+1);
-  var y = Math.floor(Math.random()*300+1);
+  var x = Math.floor(Math.random()*750+1);
+  var y = Math.floor(Math.random()*750+1);
   var r=Math.floor(Math.random()*20+1);
   var animation=Raphael.animation({cx:x,cy:y},1000,"bounce");
   this.attr("r",r);
