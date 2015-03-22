e7855a987d3a4031f59ed2c1db09a4bdaf0f0776
abraithw <arb79@duke.edu>
2015-03-21 23:35:31 -0400

    Create sick.js

---
+++ sick.js
@@ -0,0 +1,11 @@
+function short(el) {
+  el.style.color="red";
+  el.style.width= "300px";
+  
+  var myElements = document.querySelectorAll(".thrd-block");
+  for (var i = 0; i < myElements.length; i++) {
+    myElements[i].style.opacity = 1;
+  }
+  var bye=document.getElementById("long");
+  bye.style.opacity=0;
+}
