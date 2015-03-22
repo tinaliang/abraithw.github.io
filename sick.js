c8f535e333a7be13825957da8d960e4f67bf5305
abraithw <arb79@duke.edu>
2015-03-21 23:59:34 -0400

    Update sick.js

--- sick.js
+++ sick.js
@@ -2,10 +2,24 @@ function short(el) {
   el.style.color="red";
   el.style.width= "80%";
   
-  var myElements = document.querySelectorAll(".thrd-block");
+  var myElements = document.querySelectorAll(".tbshort");
   for (var i = 0; i < myElements.length; i++) {
     myElements[i].style.opacity = 1;
   }
   var bye=document.getElementById("long");
   bye.style.opacity=0;
+}
+
+
+function long(el) {
+  el.style.color="red";
+  el.style.width= "80%";
+  
+  var myElements = document.querySelectorAll(".tblong");
+  for (var i = 0; i < myElements.length; i++) {
+    myElements[i].style.opacity = 1;
+  }
+  var bye=document.getElementById("short");
+  bye.style.opacity=0;
+  
 }
